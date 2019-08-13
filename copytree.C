2d13324588e3c6c62633483cacfaed8bffbf6781
Marcel Kunze <marcel.kunze@urz.uni-heidelberg.de>
2019-08-13 08:49:05 +0200

    split particles

--- copytree.C
+++ copytree.C
@@ -1,21 +1,39 @@
 void copytree() {
-   gSystem->Load("$ROOTSYS/test/libEvent");
-   //Get old file, old tree and set top branch address
-   TFile *oldfile;
-   oldfile = new TFile("PidTuple.root");
-   TTree *oldtree = (TTree*)oldfile->Get("PidTuple");
-   //Event *event   = new Event();
-   //oldtree->SetBranchAddress("event",&event);
-   //oldtree->SetBranchStatus("*",0);
-   //oldtree->SetBranchStatus("event",1);
-   //oldtree->SetBranchStatus("fNtrack",1);
-   //oldtree->SetBranchStatus("fNseg",1);
-   //oldtree->SetBranchStatus("fH",1);
-   //Create a new file + a clone of old tree in new file
-   TFile *newfile = new TFile("small.root","recreate");
-   TTree *newtree = oldtree->CloneTree();
-   newtree->Print();
-   newfile->Write();
-   delete oldfile;
-   delete newfile;
+    TFile *oldfile;
+    oldfile = new TFile("PidTuple.root");
+    TTree *oldtree = (TTree*)oldfile->Get("PidTuple");
+    TFile *newfile = new TFile("pid.root","recreate");
+    TTree *newtree = oldtree->CloneTree();
+    
+    Float_t pid,charge,electron,muon,pion,kaon,proton;
+    newtree->SetBranchAddress("pid",&pid);
+    TBranch *cBranch = newtree->Branch("charge",&charge,"charge/F");
+    TBranch *eBranch = newtree->Branch("electron",&electron,"electron/F");
+    TBranch *mBranch = newtree->Branch("muon",&muon,"muon/F");
+    TBranch *pBranch = newtree->Branch("pion",&pion,"pion/F");
+    TBranch *kBranch = newtree->Branch("kaon",&kaon,"kaon/F");
+    TBranch *prBranch = newtree->Branch("proton",&proton,"proton/F");
+    
+    Int_t nentries = (Int_t)newtree->GetEntries();
+    for (Int_t i=0; i<nentries; i++) {
+        newtree->GetEntry(i);
+        charge = 1.0;
+        if (pid<0) charge = -1.0;
+        electron = (abs(pid) == 1);
+        muon = (abs(pid) == 2);
+        pion = (abs(pid) == 3);
+        kaon = (abs(pid) == 4);
+        proton = (abs(pid) == 5);
+        cBranch->Fill();
+        eBranch->Fill();
+        mBranch->Fill();
+        pBranch->Fill();
+        kBranch->Fill();
+        prBranch->Fill();
+    }
+    
+    newtree->Print();
+    newfile->Write();
+    delete oldfile;
+    delete newfile;
 }
