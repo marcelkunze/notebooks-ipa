74c4b7bf92118341a5f799fade73210a1c24f073
Marcel Kunze <marcel.kunze@urz.uni-heidelberg.de>
2019-08-13 09:12:44 +0200

    pidvec

--- copytree.C
+++ copytree.C
@@ -5,7 +5,7 @@ void copytree() {
     TFile *newfile = new TFile("pid.root","recreate");
     TTree *newtree = oldtree->CloneTree();
     
-    Float_t pid,charge,electron,muon,pion,kaon,proton;
+    Float_t pid,pidvec[5],charge,electron,muon,pion,kaon,proton;
     newtree->SetBranchAddress("pid",&pid);
     TBranch *cBranch = newtree->Branch("charge",&charge,"charge/F");
     TBranch *eBranch = newtree->Branch("electron",&electron,"electron/F");
@@ -13,23 +13,25 @@ void copytree() {
     TBranch *pBranch = newtree->Branch("pion",&pion,"pion/F");
     TBranch *kBranch = newtree->Branch("kaon",&kaon,"kaon/F");
     TBranch *prBranch = newtree->Branch("proton",&proton,"proton/F");
-    
+    TBranch *vBranch = newtree->Branch("pidvec",&pidvec,"pidvec[5]/F");
+
     Int_t nentries = (Int_t)newtree->GetEntries();
     for (Int_t i=0; i<nentries; i++) {
         newtree->GetEntry(i);
         charge = 1.0;
         if (pid<0) charge = -1.0;
-        electron = (abs(pid) == 1);
-        muon = (abs(pid) == 2);
-        pion = (abs(pid) == 3);
-        kaon = (abs(pid) == 4);
-        proton = (abs(pid) == 5);
+        pidvec[0] = electron = (abs(pid) == 1);
+        pidvec[1] = muon = (abs(pid) == 2);
+        pidvec[2] = pion = (abs(pid) == 3);
+        pidvec[3] = kaon = (abs(pid) == 4);
+        pidvec[4] = proton = (abs(pid) == 5);
         cBranch->Fill();
         eBranch->Fill();
         mBranch->Fill();
         pBranch->Fill();
         kBranch->Fill();
         prBranch->Fill();
+        vBranch->Fill();
     }
     
     newtree->Print();
